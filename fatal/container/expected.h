e44f95580dec0f6f1e3e488a822d55c815375857
Marcelo Juchem <juchem@gmail.com>
2019-06-14 15:53:29 -0700

    implementing `expected` helper for return-based error handling

---
+++ fatal/container/expected.h
@@ -0,0 +1,350 @@
+/*
+ *  Copyright (c) 2019, Marcelo Juchem
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree.
+ */
+
+#ifndef FATAL_INCLUDE_fatal_container_expected_h
+#define FATAL_INCLUDE_fatal_container_expected_h
+
+#include <fatal/type/safe_overload.h>
+#include <fatal/type/traits.h>
+
+#include <exception>
+#include <memory>
+#include <stdexcept>
+#include <type_traits>
+#include <variant>
+
+#include <cstdlib>
+#include <cassert>
+
+namespace fatal {
+
+////////////////////
+// error policies //
+////////////////////
+
+/**
+ * An error policy for `expected` for throwing unexpected errors (e.g.: exceptions).
+ *
+ * Author: Marcelo Juchem <juchem at gmail dot com>
+ */
+template <typename Error>
+struct throwable_expect_error_policy {
+  using error_type = Error;
+
+  /**
+   * Throws the given error.
+   *
+   * This function should not return.
+   */
+  [[ noreturn ]]
+  static void raise(error_type const &error) {
+    throw error;
+  }
+
+  /**
+   * Tells whether this is a throwing error policy or not.
+   */
+  static constexpr bool throwing() { return true; }
+};
+
+/**
+ * An error policy for `expected` for value-based errors (e.g.: error codes)
+ * which doesn't support throwing..
+ *
+ * Author: Marcelo Juchem <juchem at gmail dot com>
+ */
+template <typename Error>
+struct error_value_expect_error_policy {
+  using error_type = Error;
+
+  /**
+   * Tells whether this is a throwing error policy or not.
+   */
+  static constexpr bool throwing() { return false; }
+};
+
+/**
+ * Resolves to either `throwable_expect_error_policy` if `Error` is a subclass of `std::exception`
+ * or `error_value_expect_error_policy` otherwise.
+ *
+ * Author: Marcelo Juchem <juchem at gmail dot com>
+ */
+template <typename Error>
+using default_expect_error_policy = std::conditional_t<
+  std::is_base_of_v<std::exception, Error>,
+  throwable_expect_error_policy<Error>,
+  error_value_expect_error_policy<Error>
+>;
+
+////////////////////
+// check policies //
+////////////////////
+
+/**
+ * A check policy for `expected` that asserts pre-conditions.
+ *
+ * Author: Marcelo Juchem <juchem at gmail dot com>
+ */
+struct assert_expect_check_policy {
+  /**
+   * Asserts that `expected` contains a value.
+   */
+  template <typename Expected>
+  static void check_is_value(Expected const &expected) {
+    assert(expected.has_value());
+  }
+
+  /**
+   * Asserts that `expected` contains an error.
+   */
+  template <typename Expected>
+  static void check_is_error(Expected const &expected) {
+    assert(!expected.has_value());
+  }
+};
+
+/**
+ * A check policy for `expected` that throws on failed pre-conditions.
+ *
+ * Author: Marcelo Juchem <juchem at gmail dot com>
+ */
+struct throwing_expect_check_policy {
+  template <typename Expected>
+  /**
+   * Checks that `expected` contains a value and throws a `std::logic_error` if not.
+   */
+  static void check_is_value(Expected const &expected) {
+    if (!expected.has_value()) {
+      throw std::logic_error("attempting to retrieve the value out of an unexpected error");
+    }
+  }
+
+  /**
+   * Checks that `expected` contains an error and throws a `std::logic_error` if not.
+   */
+  template <typename Expected>
+  static void check_is_error(Expected const &expected) {
+    if (expected.has_value()) {
+      throw std::logic_error("attempting to retrieve the error out of an expected value");
+    }
+  }
+};
+
+/**
+ * A tag for unexcpected error conditions.
+ *
+ * Used in conjunction with `expected`.
+ *
+ * Author: Marcelo Juchem <juchem at gmail dot com>
+ */
+struct unexpected_t {};
+
+constexpr unexpected_t unexpected = {};
+
+/**
+ * A wrapper of return values for return-based error handling.
+ *
+ * Allows the representation of both expected return values and unexpected error conditions.
+ *
+ * Author: Marcelo Juchem <juchem at gmail dot com>
+ */
+template <
+  typename T,
+  typename Error,
+  template <typename> typename ErrorPolicy = default_expect_error_policy,
+  typename CheckPolicy = assert_expect_check_policy
+>
+struct expected {
+  using value_type = T;
+  using error_type = Error;
+  using error_policy = ErrorPolicy<Error>;
+  using check_policy = CheckPolicy;
+
+private:
+  struct error_t {
+    template <typename... UArgs, typename = fatal::safe_overload<error_t, UArgs...>>
+    explicit error_t(UArgs &&...args):
+      error_(std::forward<UArgs>(args)...)
+    {}
+
+    error_t(error_t const &) = default;
+    error_t(error_t &&) = default;
+
+    /**
+     * Getter for the wrapped error.
+     */
+    auto const &get() const { return error_; }
+
+    /**
+     * Getter for the wrapped error.
+     */
+    auto &get() { return error_; }
+
+  private:
+    error_type error_;
+  };
+
+public:
+
+  /**
+   * Constructs an `expected` with inplace construction of value from the given arguments.
+   */
+  /* implicit */
+  template <
+    typename... UArgs,
+    typename = fatal::safe_overload<expected, UArgs...>,
+    typename = std::enable_if_t<
+      sizeof...(UArgs) != 1
+        || !(std::is_same_v<unexpected_t, UArgs> && ... && true)
+    >
+  >
+  expected(UArgs &&...args):
+    data_(std::in_place_type<value_type>, std::forward<UArgs>(args)...)
+  {}
+
+  /**
+   * Constructs an `expected` with an unexpected error.
+   */
+  template <typename... UArgs>
+  expected(unexpected_t, UArgs &&...args):
+    data_(std::in_place_type<error_t>, std::forward<UArgs>(args)...)
+  {}
+
+  expected(expected const &) = default;
+  expected(expected &&) = default;
+
+  /**
+   * Getter for the wrapped value.
+   *
+   * Calls the check policy to ensure this object contains a value.
+   */
+  value_type const &value() const {
+    check_policy::check_is_value(*this);
+    return std::get<value_type>(data_);
+  }
+
+  /**
+   * Getter for the wrapped value.
+   *
+   * Calls the check policy to ensure this object contains a value.
+   */
+  value_type &value() {
+    check_policy::check_is_value(*this);
+    return std::get<value_type>(data_);
+  }
+
+  /**
+   * Conditional getter for the wrapped value.
+   *
+   * Returns a pointer to the value if present, or `nullptr` otherwise.
+   */
+  value_type const *try_value() const {
+    if (!has_value()) { return nullptr; }
+    return std::addressof(value());
+  }
+
+  /**
+   * Conditional getter for the wrapped value.
+   *
+   * Returns a pointer to the value if present, or `nullptr` otherwise.
+   */
+  value_type *try_value() {
+    if (!has_value()) { return nullptr; }
+    return std::addressof(value());
+  }
+
+  /**
+   * Getter for the wrapped error.
+   *
+   * Calls the check policy to ensure this object contains an error.
+   */
+  error_type const &error() const {
+    check_policy::check_is_error(*this);
+    return std::get<error_t>(data_).get();
+  }
+
+  /**
+   * Getter for the wrapped error object.
+   *
+   * Calls the check policy to ensure this object contains an error.
+   */
+  error_type &error() {
+    check_policy::check_is_error(*this);
+    return std::get<error_t>(data_).get();
+  }
+
+  /**
+   * Conditional getter for the wrapped error.
+   *
+   * Returns a pointer to the error if present, or `nullptr` otherwise.
+   */
+  error_type const *try_error() const {
+    if (has_value()) { return nullptr; }
+    return std::addressof(error());
+  }
+
+  /**
+   * Conditional getter for the wrapped error.
+   *
+   * Returns a pointer to the error if present, or `nullptr` otherwise.
+   */
+  error_type *try_error() {
+    if (has_value()) { return nullptr; }
+    return std::addressof(error());
+  }
+
+  /**
+   * Returns `true` if a value is present, otherwise returns `false` as an error is.
+   */
+  bool has_value() const { return !data_.index(); }
+
+  /**
+   * This method can't be called on non-throwable 
+   */
+  [[ noreturn ]]
+  void raise() const {
+    check_policy::check_is_error(*this);
+    if constexpr (throwing()) {
+      error_policy::raise(error());
+    } else {
+      static_assert(throwing(), "can't raise on a non-throwing error policy");
+    }
+
+    // ensure we won't return in case of a misbehaving error policy
+    std::abort();
+  }
+
+  void try_raise() const {
+    if (has_value()) { return; }
+    raise();
+  }
+
+  explicit operator bool() const { return has_value(); }
+  bool operator !() const { return !has_value(); }
+
+  constexpr static bool throwing() { return error_policy::throwing(); }
+
+private:
+  std::variant<value_type, error_t> data_;
+};
+
+/**
+ * Convenient alias for an `expected` that uses `throwing_expect_check_policy`.
+ *
+ * Author: Marcelo Juchem <juchem at gmail dot com>
+ */
+template <
+  typename T,
+  typename Error,
+  template <typename> typename ErrorPolicy = default_expect_error_policy
+>
+using checked_expected = expected<T, Error, ErrorPolicy, throwing_expect_check_policy>;
+
+} // namespace fatal {
+
+#endif // FATAL_INCLUDE_fatal_container_expected_h
---
+++ fatal/container/test/expected_test.cpp
@@ -0,0 +1,382 @@
+/*
+ *  Copyright (c) 2019, Marcelo Juchem
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+#include <fatal/container/expected.h>
+
+#include <fatal/test/driver.h>
+
+#include <vector>
+#include <unordered_map>
+
+namespace fatal {
+
+struct custom_error:
+  public std::exception
+{
+  custom_error() = default;
+
+  template <typename... Args>
+  explicit custom_error(std::string_view message, Args &&...args):
+    message_(message),
+    code_((0 + ... + args))
+  {}
+
+  custom_error(custom_error const &) = default;
+  custom_error(custom_error &&) = default;
+
+  bool operator !=(custom_error const &rhs) const { return !(*this == rhs); }
+  bool operator ==(custom_error const &rhs) const {
+    return message_ == rhs.message_ && code_ == rhs.code_;
+  }
+
+private:
+  std::string_view const message_;
+  int const code_ = 0;
+};
+
+FATAL_TEST(expected, success example) {
+  // simulates a function that returns a success value through `expected`
+  auto computation = []() -> expected<int, custom_error> {
+    return 10;
+  };
+
+  FATAL_EXPECT_NO_THROW {
+    auto result = computation();
+
+    if (!result) {
+      FATAL_EXPECT_UNREACHABLE();
+    } else {
+      FATAL_EXPECT_EQ(10, result.value());
+    }
+
+    result.try_raise();
+  };
+}
+
+FATAL_TEST(expected, success with arguments example) {
+  // simulates a function that returns a success value constructed with arguments through `expected`
+  auto computation = []() -> expected<std::string, custom_error> {
+    return {"test string"};
+  };
+
+  FATAL_EXPECT_NO_THROW {
+    auto result = computation();
+
+    if (!result) {
+      FATAL_EXPECT_UNREACHABLE();
+    } else {
+      FATAL_EXPECT_EQ("test string", result.value());
+    }
+
+    result.try_raise();
+  };
+}
+
+FATAL_TEST(expected, success with multiple arguments example) {
+  // simulates a function that returns a success value constructed with multiple arguments
+  // through `expected`
+  auto computation = []() -> expected<std::pair<std::string, int>, custom_error> {
+    return {"test string", 100};
+  };
+
+  FATAL_EXPECT_NO_THROW {
+    auto result = computation();
+
+    if (!result) {
+      FATAL_EXPECT_UNREACHABLE();
+    } else {
+      FATAL_EXPECT_EQ("test string", result.value().first);
+      FATAL_EXPECT_EQ(100, result.value().second);
+    }
+
+    result.try_raise();
+  };
+}
+
+FATAL_TEST(expected, failure code example) {
+  // simulates a function that returns an error code through `expected`
+  auto computation = []() -> expected<int, long> {
+    return {unexpected, -1};
+  };
+
+  FATAL_EXPECT_NO_THROW {
+    auto result = computation();
+
+    if (!result) {
+      FATAL_EXPECT_EQ(-1, result.error());
+    } else {
+      FATAL_EXPECT_UNREACHABLE();
+    }
+
+    // we can't call `result.raise()` nor `result.try_raise()` because that would fail
+    // to compile - this is intended because the default policy for non-exception error
+    // types is to not allow throwing
+  };
+}
+
+FATAL_TEST(expected, default-constructed failure example) {
+  // simulates a function that returns a default constructed error through `expected`
+  auto computation = []() -> expected<int, custom_error> {
+    return unexpected;
+  };
+
+  FATAL_EXPECT_THROW(custom_error) {
+    auto result = computation();
+
+    if (!result) {
+      FATAL_EXPECT_EQ(custom_error(), result.error());
+
+      result.raise();
+    } else {
+      FATAL_EXPECT_UNREACHABLE();
+    }
+  };
+
+  FATAL_EXPECT_THROW(custom_error) {
+    auto result = computation();
+
+    result.try_raise();
+
+    FATAL_EXPECT_UNREACHABLE();
+  };
+}
+
+FATAL_TEST(expected, default-constructed success example) {
+  // simulates a function that returns a default constructed value through `expected`
+  auto computation = []() -> expected<int, custom_error> {
+    return {};
+  };
+
+  FATAL_EXPECT_NO_THROW {
+    auto result = computation();
+
+    if (!result) {
+      FATAL_EXPECT_UNREACHABLE();
+    } else {
+      FATAL_EXPECT_EQ(int{}, result.value());
+    }
+
+    result.try_raise();
+  };
+}
+
+FATAL_TEST(expected, failure with arguments example) {
+  // simulates a function that returns an error constructed with arguments through `expected`
+  auto computation = []() -> expected<int, custom_error> {
+    return {unexpected, "error message"};
+  };
+
+  FATAL_EXPECT_THROW(custom_error) {
+    auto result = computation();
+
+    if (!result) {
+      FATAL_EXPECT_EQ(custom_error("error message"), result.error());
+
+      result.raise();
+    } else {
+      FATAL_EXPECT_UNREACHABLE();
+    }
+  };
+
+  FATAL_EXPECT_THROW(custom_error) {
+    auto result = computation();
+
+    result.try_raise();
+
+    FATAL_EXPECT_UNREACHABLE();
+  };
+}
+
+FATAL_TEST(expected, failure with multiple arguments example) {
+  // simulates a function that returns an error constructed with multiple arguments
+  // through `expected`
+  auto computation = []() -> expected<int, custom_error> {
+    return {unexpected, "error message", 1, 2, 3};
+  };
+
+  FATAL_EXPECT_THROW(custom_error) {
+    auto result = computation();
+
+    if (!result) {
+      FATAL_EXPECT_EQ(custom_error("error message", 1, 2, 3), result.error());
+
+      result.raise();
+    } else {
+      FATAL_EXPECT_UNREACHABLE();
+    }
+  };
+
+  FATAL_EXPECT_THROW(custom_error) {
+    auto result = computation();
+
+    result.try_raise();
+
+    FATAL_EXPECT_UNREACHABLE();
+  };
+}
+
+FATAL_TEST(expected, default_ctor int / int) {
+  expected<int, int> e;
+
+  FATAL_EXPECT_TRUE(e.has_value());
+  FATAL_EXPECT_NOT_NULL(e.try_value());
+
+  FATAL_EXPECT_NULL(e.try_error());
+}
+
+FATAL_TEST(expected, value int / int) {
+  expected<int, int> e(10);
+
+  FATAL_EXPECT_TRUE(e.has_value());
+
+  FATAL_EXPECT_EQ(10, e.value());
+
+  FATAL_ASSERT_NOT_NULL(e.try_value());
+  FATAL_EXPECT_EQ(10, *e.try_value());
+
+  FATAL_EXPECT_NULL(e.try_error());
+}
+
+FATAL_TEST(expected, error int / int) {
+  expected<int, int> e(unexpected, 5);
+
+  FATAL_EXPECT_FALSE(e.has_value());
+
+  FATAL_ASSERT_NULL(e.try_value());
+
+  FATAL_EXPECT_NOT_NULL(e.try_error());
+  FATAL_EXPECT_EQ(5, *e.try_error());
+}
+
+
+
+FATAL_TEST(expected, default_ctor int / custom_error) {
+  expected<int, custom_error> e;
+
+  FATAL_EXPECT_TRUE(e.has_value());
+  FATAL_EXPECT_NOT_NULL(e.try_value());
+
+  FATAL_EXPECT_NULL(e.try_error());
+}
+
+FATAL_TEST(expected, value int / custom_error) {
+  expected<int, custom_error> e(10);
+
+  FATAL_EXPECT_TRUE(e.has_value());
+
+  FATAL_EXPECT_EQ(10, e.value());
+
+  FATAL_ASSERT_NOT_NULL(e.try_value());
+  FATAL_EXPECT_EQ(10, *e.try_value());
+
+  FATAL_EXPECT_NULL(e.try_error());
+}
+
+FATAL_TEST(expected, error int / custom_error) {
+  expected<int, custom_error> e(unexpected, "message");
+
+  FATAL_EXPECT_FALSE(e.has_value());
+
+  FATAL_ASSERT_NULL(e.try_value());
+
+  FATAL_EXPECT_NOT_NULL(e.try_error());
+  FATAL_EXPECT_EQ(custom_error("message"), *e.try_error());
+
+  FATAL_EXPECT_THROW(custom_error) { e.raise(); };
+}
+
+
+
+FATAL_TEST(expected, default_ctor custom_error / int) {
+  expected<custom_error, int> e;
+
+  FATAL_EXPECT_TRUE(e.has_value());
+  FATAL_EXPECT_NOT_NULL(e.try_value());
+
+  FATAL_EXPECT_NULL(e.try_error());
+}
+
+FATAL_TEST(expected, value custom_error / int) {
+  expected<custom_error, int> e("result");
+
+  FATAL_EXPECT_TRUE(e.has_value());
+
+  FATAL_EXPECT_EQ(custom_error("result"), e.value());
+
+  FATAL_ASSERT_NOT_NULL(e.try_value());
+  FATAL_EXPECT_EQ(custom_error("result"), *e.try_value());
+
+  FATAL_EXPECT_NULL(e.try_error());
+}
+
+FATAL_TEST(expected, error custom_error / int) {
+  expected<custom_error, int> e(unexpected, 5);
+
+  FATAL_EXPECT_FALSE(e.has_value());
+
+  FATAL_ASSERT_NULL(e.try_value());
+
+  FATAL_EXPECT_NOT_NULL(e.try_error());
+  FATAL_EXPECT_EQ(5, *e.try_error());
+}
+
+
+
+FATAL_TEST(expected, default_ctor custom_error / custom_error) {
+  expected<custom_error, custom_error> e;
+
+  FATAL_EXPECT_TRUE(e.has_value());
+  FATAL_EXPECT_NOT_NULL(e.try_value());
+
+  FATAL_EXPECT_NULL(e.try_error());
+}
+
+FATAL_TEST(expected, value custom_error / custom_error) {
+  expected<custom_error, custom_error> e("result");
+
+  FATAL_EXPECT_TRUE(e.has_value());
+
+  FATAL_EXPECT_EQ(custom_error("result"), e.value());
+
+  FATAL_ASSERT_NOT_NULL(e.try_value());
+  FATAL_EXPECT_EQ(custom_error("result"), *e.try_value());
+
+  FATAL_EXPECT_NULL(e.try_error());
+}
+
+FATAL_TEST(expected, error custom_error / custom_error) {
+  expected<custom_error, custom_error> e(unexpected, "message");
+
+  FATAL_EXPECT_FALSE(e.has_value());
+
+  FATAL_ASSERT_NULL(e.try_value());
+
+  FATAL_EXPECT_NOT_NULL(e.try_error());
+  FATAL_EXPECT_EQ(custom_error("message"), *e.try_error());
+
+  FATAL_EXPECT_THROW(custom_error) { e.raise(); };
+}
+
+
+
+FATAL_TEST(expected, value checked_expected) {
+  checked_expected<int, int> e;
+
+  FATAL_EXPECT_NO_THROW { e.value(); };
+  FATAL_EXPECT_THROW(std::logic_error) { e.error(); };
+}
+
+FATAL_TEST(expected, error checked_expected) {
+  checked_expected<int, int> e(unexpected);
+
+  FATAL_EXPECT_THROW(std::logic_error) { e.value(); };
+  FATAL_EXPECT_NO_THROW { e.error(); };
+}
+
+} // namespace fatal {
