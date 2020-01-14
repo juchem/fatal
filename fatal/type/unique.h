93ffccc6db33c18ec1fcfc7cd72aaec0a8e81c23
Marcelo Juchem <juchem@gmail.com>
2020-01-14 09:39:08 -0800

    adding is_adjacent_unique/is_adjacent_unique_v/is_adjacent_unique_by/is_adjacent_unique_by_v

--- fatal/type/unique.h
+++ fatal/type/unique.h
@@ -22,6 +22,18 @@ using adjacent_unique = typename i_uq::au<T>::type;
 template <typename T, typename Comparer>
 using adjacent_unique_by = typename i_uq::au<T, Comparer>::type;
 
+template <typename T>
+using is_adjacent_unique = std::is_same<T, adjacent_unique<T>>;
+
+template <typename T>
+constexpr bool is_adjacent_unique_v = std::is_same_v<T, adjacent_unique<T>>;
+
+template <typename T, typename Comparer>
+using is_adjacent_unique_by = std::is_same<T, adjacent_unique_by<T, Comparer>>;
+
+template <typename T, typename Comparer>
+constexpr bool is_adjacent_unique_by_v = std::is_same_v<T, adjacent_unique_by<T, Comparer>>;
+
 } // namespace fatal {
 
 #endif // FATAL_INCLUDE_fatal_type_unique_h
